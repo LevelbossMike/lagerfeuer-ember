9429699ad8dec64653f08f0005ab911603ee7bd4
Michael Klein <michael@firstiwaslike.com>
2014-01-03 14:10:57 +0100

    Add: Automatically highlight code snippets.

--- app/helpers/markdown-text.js
+++ app/helpers/markdown-text.js
@@ -1,3 +1,9 @@
+marked.setOptions({
+  highlight: function(code) {
+    return hljs.highlightAuto(code).value;
+  }
+});
+
 export default Ember.Handlebars.makeBoundHelper(function(input) {
   if (input != null) {
     var markdown = marked(input);
